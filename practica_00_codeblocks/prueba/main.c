f0e018678f58effa1ea15c4f4639481befd8983f
Mauro Cuquejo <mcuquejo@fi.uba.ar>
2019-09-04 22:45:41 -0300

    se agrega nuevo tp

---
+++ practica_00_codeblocks/prueba/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    int prueba;
+    for (int i = 0; i < 5; i++){
+        prueba = i;
+        printf("prueba: %d", i);
+    }
+    return 0;
+}
